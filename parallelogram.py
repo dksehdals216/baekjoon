8098dbcdd2cd13bdd69032a39bee18ac421121f1
dksehdals216 <21600397@handong.edu>
2018-07-06 17:22:31 +0900

    modified parallelogram.py

--- parallelogram.py
+++ parallelogram.py
@@ -1,4 +1,21 @@
 
 #1064
+'''
+all inp are smaller or eq to 5000
+
+there can be multiple D values, find difference between
+largest and smallest parallelogram circumference
+
+
+if no parallelogram is possible, print -1
+'''
+
+
+if __name__ == '__main__':
+    inp = int(input())
+
+
+
+
 
 
